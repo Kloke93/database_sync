6a990d1437c26360a51fd87749da90dec97b1a33
tomas dal farra <tomasdfarra@gmail.com>
2022-12-28 01:15:34 +0200

    final project
    finished tests and correct the not reading event bug

--- sync_database.py
+++ sync_database.py
@@ -1,7 +1,7 @@
 """
 Author: Tomas Dal Farra
-Date:
-Description:
+Date: 25/12/2022
+Description: Synchronized database class for threads and processes
 """
 from file_database import FileDataBase
 import threading
@@ -13,7 +13,10 @@ class SyncDataBase(FileDataBase):
     """
     Simple database thread/process synchronized
     """
+    # All the not_reading event part was commented out because it didn't work.
     READERS_BOUND = 10
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)              # set the minimum logger level
 
     def __init__(self, mode, file_name="dbfile.bin"):
         """
@@ -24,46 +27,31 @@ class SyncDataBase(FileDataBase):
         if mode != 0 and mode != 1:
             raise TypeError("Not Corresponding type")
         super().__init__(file_name)
-        self.counter_readers = 0                 # readers count
+        # self.counter_readers = 0                                      # readers count
         if mode:
-            self.not_reading = threading.Event()                    # Event to check if there are readers
-            self.not_writing = threading.Event()                    # Event to check if there are writers
-            self.count_lock = threading.Lock()                      # Lock to change readers count
-            self.to_write_lock = threading.Lock()                   # Lock to writers
-            self.readers_semaphore = threading.Semaphore(SyncDataBase.READERS_BOUND)    # Semaphore to limit readers
+            self.not_writing = threading.Event()                        # Event to check if there are writers
+            self.to_write_lock = threading.Lock()                       # Lock to writers
+            self.semaphore = threading.Semaphore(SyncDataBase.READERS_BOUND)    # Semaphore to limit readers
+            # self.count_lock = threading.Lock()                        # Lock to change readers count
+            # self.not_reading = threading.Event()                      # Event to check if there are readers
             # logging format
-            log_format = "[%(filename)s][%(threadName)s][%(asctime)s] %(message)s"
+            formatter = logging.Formatter("[%(filename)s][%(threadName)s][%(asctime)s] %(message)s")
         else:                       # same attributes but in the multiprocessing module
-            self.not_reading = multiprocessing.Event()
             self.not_writing = multiprocessing.Event()
-            self.count_lock = multiprocessing.Lock()
             self.to_write_lock = multiprocessing.Lock()
-            self.readers_semaphore = multiprocessing.Semaphore(10)
+            self.semaphore = multiprocessing.Semaphore(10)
+            # self.count_lock = multiprocessing.Lock()
+            # self.not_reading = multiprocessing.Event()
             # logging format
-            log_format = "[%(filename)s][%(processName)s][%(asctime)s] %(message)s"
+            formatter = logging.Formatter("[%(filename)s][%(processName)s][%(asctime)s] %(message)s")
+        # logger configuration
+        file_handler = logging.FileHandler("file_database.log")         # file to save the log
+        file_handler.setFormatter(formatter)
+        SyncDataBase.logger.addHandler(file_handler)
+        SyncDataBase.logger.info(f"Start in mode {mode}")
         # setting not-events
         self.not_writing.set()
-        self.not_reading.set()
-        # logging config
-        log_file = "file_database.log"                  # file to save the log
-        log_level = logging.DEBUG                       # set the minimum logger level
-        logging.basicConfig(filename=log_file, level=log_level, format=log_format)
-        logging.info("started logging")
-
-    def _update_counter(self, factor: int):
-        """
-        Updates reading counter according to if reader acquired or released
-        :param factor: 1 if acquired and -1 if released
-        """
-        self.count_lock.acquire()
-        try:
-            self.counter_readers += factor
-        finally:
-            self.count_lock.release()
-        if self.counter_readers:
-            self.not_reading.clear()
-        else:
-            self.not_reading.set()
+        # self.not_reading.set()
 
     def set_value(self, key, val) -> bool:
         """
@@ -73,13 +61,19 @@ class SyncDataBase(FileDataBase):
         :return: If the operation was successful
         """
         self.to_write_lock.acquire()                        # blocks for writers
+        self.not_writing.clear()                            # event: someone wants to write
+        for i in range(10):                                 # fills all the semaphore buffer to write
+            self.semaphore.acquire()
         try:
-            self.not_writing.clear()                        # event: someone wants to write
-            self.not_reading.wait()                         # waits for readers to finish
+            # self.not_reading.wait()                         # waits for readers to finish
             is_set = super().set_value(key, val)
-            logging.debug(f"({key}:{val}) {is_set} is set")
-            self.not_writing.set()                          # event: stopped writing
+        except Exception as err:
+            SyncDataBase.logger.error(f"Error setting key<{key}> to value<{val}>: {err}")
+            raise err
         finally:
+            for i in range(10):                             # releases all the semaphore buffer
+                self.semaphore.release()
+            self.not_writing.set()                          # event: stopped writing
             self.to_write_lock.release()                    # release for writers
         return is_set
 
@@ -91,14 +85,16 @@ class SyncDataBase(FileDataBase):
         :return: Value from the database if found
         """
         self.not_writing.wait()                             # wait for writer to finish
-        self.readers_semaphore.acquire()                    # acquires semaphore to read
-        self._update_counter(1)                             # increases reader counter
+        # self._update_counter(1)                           # increases reader counter
+        self.semaphore.acquire()                            # acquires semaphore to read
         try:
             val = super().get_value(key)
-            logging.debug(f"by key<{key}> got value<{val}>")
+        except Exception as err:
+            SyncDataBase.logger.error(f"Error getting key<{key}>: {err}")
+            raise err
         finally:
-            self.readers_semaphore.release()                # releases semaphore
-            self._update_counter(-1)                        # decreases reader counter
+            self.semaphore.release()                        # releases semaphore
+            # self._update_counter(-1)                      # decreases reader counter
         return val
 
     def delete_value(self, key):
@@ -108,20 +104,54 @@ class SyncDataBase(FileDataBase):
         :return: Deleted value if existed
         """
         self.to_write_lock.acquire()                        # blocks for writers
+        for i in range(10):                                 # fills all the semaphore buffer to write
+            self.semaphore.acquire()
         try:
-            self.not_reading.wait()                         # event: someone wants to write
+            # self.not_reading.wait()                       # event: someone wants to write
             self.not_writing.clear()                        # waits for readers to finish
             deleted = super().delete_value(key)
-            logging.debug(f"by key<{key}> deleted value<{deleted}>")
-            self.not_writing.set()                          # event: stopped writing
+        except Exception as err:
+            SyncDataBase.logger.error(f"Error deleting key<{key}>: {err}")
+            raise err
         finally:
+            for i in range(10):                             # releases all the semaphore buffer
+                self.semaphore.release()
+            self.not_writing.set()                          # event: stopped writing
             self.to_write_lock.release()                    # release for writers
         return deleted
 
+    def _set_value_testing(self, key) -> bool:
+        """ Special set_value modification to change previous value of key in dictionary by one"""
+        self.to_write_lock.acquire()                        # blocks for writers
+        self.not_writing.clear()                            # event: someone wants to write
+        for i in range(10):                                 # fills all the semaphore buffer to write
+            self.semaphore.acquire()
+        try:
+            # self.not_reading.wait()                       # waits for readers to finish
+            val = super().get_value(key)
+            is_set = super().set_value(key, val + 1)
+        except Exception as err:
+            SyncDataBase.logger.error(f"Error setting (test) key<{key}>: {err}")
+            raise err
+        finally:
+            for i in range(10):                             # releases all the semaphore buffer
+                self.semaphore.release()
+            self.not_writing.set()                          # event: stopped writing
+            self.to_write_lock.release()                    # release for writers
+        return is_set
 
-if __name__ == "__main__":
-    db = SyncDataBase(1, "testfile.bin")
-    t = threading.Thread(target=db.set_value, name="thread_ws", args=(40, 4002))
-    t.start()
-    t.join()
-    print(db)
+    # Didn't work. Ignore it
+    # def _update_counter(self, factor: int):
+    #     """
+    #     Updates reading counter according to if reader will acquire or release
+    #     :param factor: 1 if acquired and -1 if released
+    #     """
+    #     self.count_lock.acquire()
+    #     try:
+    #         self.counter_readers += factor
+    #     finally:
+    #         self.count_lock.release()
+    #     if self.counter_readers:
+    #         self.not_reading.clear()
+    #     else:
+    #         self.not_reading.set()
--- test_sync.py
+++ test_sync.py
@@ -1,9 +1,10 @@
 """
 Author: Tomas Dal Farra
-Date: 25/12/2022
+Date: 27/12/2022
 Description: Test file for threading and multiprocessing synchronized
 """
 from sync_database import SyncDataBase
+from random import randint
 import multiprocessing
 import threading
 import unittest
@@ -14,7 +15,7 @@ import os
 class TestThreadDB(unittest.TestCase):
     """ Class to test synchronized database in threading mode """
     test_fname = "testfile.bin"
-    reps = 100
+    reps = 5000
 
     def set_value(self, key, val):
         """ Test set_value method """
@@ -26,11 +27,15 @@ class TestThreadDB(unittest.TestCase):
         for _ in range(TestThreadDB.reps):
             self.assertEqual(self.sync_db.get_value(key), key * 100)
 
-    def delete_value(self, key):
-        """ Test delete_value method """
-        self.assertEqual(self.sync_db.delete_value(key), key * 100)
-        for _ in range(TestThreadDB.reps - 1):
-            self.assertIsNone(self.sync_db.delete_value(key))
+    def set_value_special(self, key):
+        """ Test set_value method based on a previous value """
+        for _ in range(TestThreadDB.reps):
+            self.assertTrue(self.sync_db._set_value_testing(key))
+
+    def get_value_exists(self, key):
+        """ Test get_value method just that returns some int """
+        for _ in range(TestThreadDB.reps):
+            self.assertIsInstance(self.sync_db.get_value(key), int)
 
     @staticmethod
     def get_database_dict():
@@ -49,9 +54,10 @@ class TestThreadDB(unittest.TestCase):
         self.test_dict = {n: n * 100 for n in range(1, 51)}
         with open(TestThreadDB.test_fname, "wb") as f:
             pickle.dump(self.test_dict, f)
-        self.sync_db = SyncDataBase(1, "testfile.bin")
+        self.sync_db = SyncDataBase(1, TestThreadDB.test_fname)
 
     def test_write_simple(self):
+        """ Tests writing without competition """
         thread = threading.Thread(target=self.set_value, name="thread_ws", args=(40, 4002))
         thread.start()
         thread.join()
@@ -59,16 +65,31 @@ class TestThreadDB(unittest.TestCase):
         self.assertEqual(self.get_database_dict(), self.test_dict)
 
     def test_read_simple(self):
+        """ Tests reading without competition """
         thread = threading.Thread(target=self.get_value, name="thread_ws", args=(40,))
         thread.start()
         thread.join()
         self.assertEqual(self.get_database_dict(), self.test_dict)
 
-    def test_write_waits(self):
-        pass
-
-    def test_read_waits(self):
-        pass
+    def test_general(self):
+        """ Tests of 12 threads trying to read and write (9 read and 3 that do both) changes correctly """
+        randbers = [randint(21, 51)] * 3          # 1 random numbers between 21 and 50. 3 times in list
+        threads = []
+        # append 9 reading threads between 3 random numbers
+        for i in range(1, 10):
+            t = threading.Thread(target=self.get_value_exists, name=f"thread_{i}", args=(randbers[0],))
+            threads.append(t)
+        # append 3 writing threads that increase number of value by 1 each iteration
+        for i in range(10, 13):
+            t = threading.Thread(target=self.set_value_special, name=f"thread_{i}", args=(randbers[0],))
+            threads.append(t)
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        for i in randbers:
+            self.test_dict[i] += TestThreadDB.reps
+        self.assertEqual(self.get_database_dict(), self.test_dict)
 
     def tearDown(self):
         """
@@ -77,9 +98,35 @@ class TestThreadDB(unittest.TestCase):
         os.remove("testfile.bin")
 
 
+#class HelpProcessTest:
+#    """ Because multiprocess can not target methods in an unittest.TestCase class I had to add this class """
+
+
 class TestProcessDB(unittest.TestCase):
     """ Class to test synchronized database in multiprocessing mode """
     test_fname = "testfile.bin"
+    reps = 5000
+
+    # In processes methods does not execute unittest asserts within themselves
+    def set_value(self, key, val):
+        """ Test set_value method """
+        for _ in range(TestProcessDB.reps):
+            self.sync_db.set_value(key, val)
+
+    def get_value(self, key):
+        """ Test get_value method """
+        for _ in range(TestProcessDB.reps):
+            self.sync_db.get_value(key), key * 100
+
+    def set_value_special(self, key):
+        """ Test set_value method based on a previous value """
+        for _ in range(TestProcessDB.reps):
+            self.sync_db._set_value_testing(key)
+
+    def get_value_exists(self, key):
+        """ Test get_value method just that returns some int """
+        for _ in range(TestProcessDB.reps):
+            self.sync_db.get_value(key), int
 
     @staticmethod
     def get_database_dict():
@@ -98,16 +145,59 @@ class TestProcessDB(unittest.TestCase):
         self.test_dict = {n: n * 100 for n in range(1, 51)}
         with open(TestProcessDB.test_fname, "wb") as f:
             pickle.dump(self.test_dict, f)
-        self.sync_db = SyncDataBase(1, "testfile.bin")
+        self.sync_db = SyncDataBase(0, TestProcessDB.test_fname)
 
-    def test_a(self):
-        pass
+    def test_write_simple(self):
+        """ Tests writing without competition """
+        proc = multiprocessing.Process(target=self.set_value, name="proc_ws", args=(40, 4002))
+        proc.start()
+        proc.join()
+        self.test_dict[40] = 4002
+        self.assertEqual(self.get_database_dict(), self.test_dict)
+
+    def test_read_simple(self):
+        """ Tests reading without competition """
+        proc = multiprocessing.Process(target=self.get_value, name="proc_ws", args=(40, 4002))
+        proc.start()
+        proc.join()
+        self.assertEqual(self.get_database_dict(), self.test_dict)
+
+    def test_general(self):
+        """ Tests of 12 processes trying to read and write (9 read and 3 that do both) changes correctly """
+        randbers = [randint(21, 51)] * 3  # 1 random numbers between 21 and 50. 3 times in list
+        procs = []
+        # append 9 reading processes between 3 random numbers
+        for i in range(1, 10):
+            p = multiprocessing.Process(target=self.get_value_exists, name=f"proc_{i}", args=(randbers[0],))
+            procs.append(p)
+        # append 3 writing processes that increase number of value by 1 each iteration
+        for i in range(10, 13):
+            p = multiprocessing.Process(target=self.set_value_special, name=f"proc_{i}", args=(randbers[0],))
+            procs.append(p)
+        for p in procs:
+            p.start()
+        for p in procs:
+            p.join()
+        for i in randbers:
+            self.test_dict[i] += TestProcessDB.reps
+        self.assertEqual(self.get_database_dict(), self.test_dict)
 
     def tearDown(self):
         """
         Deletes the testing file
         """
         os.remove("testfile.bin")
+
+    # https://stackoverflow.com/questions/25646382/python-3-4-multiprocessing-does-not-work-with-unittest
+    # Dano answer to understand
+    # __getstate__ and __setstate can be ignored
+    def __getstate__(self):
+        self_dict = self.__dict__.copy()
+        del self_dict['_outcome']
+        return self_dict
+
+    def __setstate(self, state):
+        self.__dict__.update(self_dict)
 
 
 if __name__ == "__main__":
